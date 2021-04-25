920ca3c263a52a4ca761331fdc0ec03d59e9e50c
Geniecko <radzioo159@gmail.com>
2021-04-25 15:09:54 +0200

    add styled-components

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700;800&display=swap');
+  
+  *, *::before, *::after {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin: 0;
+    padding: 0;
+  }
+  
+  html {
+    font-size: 62.5%; 
+  }
+  
+  body {
+    font-size: 1.6rem;
+    font-family: "Montserrat", sans-serif;
+    background-color: #F2F2F2;
+  }
+
+  a{
+     cursor:pointer;
+  }
+
+  button{
+     cursor:pointer;
+     font-family: "Montserrat", sans-serif;
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/theme/mainTheme.js
@@ -0,0 +1,20 @@
+export const theme = {
+  white: '#F2F2F2',
+  blue: '#1C91D3',
+  dark: '#363636',
+  gray: '#C5C5C5',
+  paragraphColor: 'rgba(54, 54, 54, 0.8);',
+  light: 300,
+  regular: 400,
+  semiBold: 600,
+  bold: 700,
+  extraBold: 800,
+  fontSize: {
+    xxs: '1.2rem',
+    xs: '1.4rem',
+    s: '1.6rem',
+    m: '2.2rem',
+    l: '2.6rem',
+    xl: '4.4rem',
+  },
+};
