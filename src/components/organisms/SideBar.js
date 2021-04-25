ffb29efd1ad2640f8e355fa9e2975a203daa8a46
Geniecko <radzioo159@gmail.com>
2021-04-25 15:10:05 +0200

    create SideBar component

---
+++ src/components/organisms/SideBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import userIcon from 'assets/icon/userIcon.svg';
+import bulbIcon from 'assets/icon/bulbIcon.svg';
+import planeIcon from 'assets/icon/planeIcon.svg';
+import NavIcon from 'components/atoms/NavIcon';
+
+const StyledNavWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  width: 170px;
+  background-color: ${({ theme }) => theme.dark};
+`;
+
+const SideBar = () => (
+  <StyledNavWrapper>
+    <NavIcon active="true" icon={userIcon}>
+      O MNIE
+    </NavIcon>
+    <NavIcon icon={bulbIcon}>PORTFOLIO</NavIcon>
+    <NavIcon icon={planeIcon}>KONTAKT</NavIcon>
+  </StyledNavWrapper>
+);
+
+export default SideBar;
