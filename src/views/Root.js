b3bbebaacb290d3d3163b97159425f047e13935e
Geniecko <radzioo159@gmail.com>
2021-04-25 15:08:54 +0200

    configure eslint and prettier, add assets folder

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React from 'react';
+import MainTemplate from 'templates/MainTemplate';
+
+import Button from 'components/atoms/Button';
+import Logo from 'components/atoms/Logo';
+import SideBar from 'components/organisms/SideBar';
 
 const Root = () => (
   <>
-    <h1>hello</h1>
+    <MainTemplate>
+      <Button>Pobierz CV</Button>
+      <Button secondary>Pobierz CV</Button>
+      <Logo>R</Logo>
+      <br />
+      <SideBar />
+    </MainTemplate>
   </>
 );
 export default Root;
