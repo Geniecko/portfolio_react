cdf7a0b6924869015a05fb7324674c5fd17d534d
Geniecko <radoslawkania09@gmail.com>
2022-06-08 21:47:45 +0200

    add new project

--- src/views/Portfolio.js
+++ src/views/Portfolio.js
@@ -8,6 +8,8 @@ import Heading from 'components/atoms/Heading';
 import Paragraph from 'components/atoms/Paragraph';
 import Button from 'components/atoms/Button';
 
+import musicFestival from 'assets/image/music_festival.jpg'
+import musicFestivalMobile from 'assets/image/music_festival_mobile.jpg'
 import thomasAnderson from 'assets/image/thomas_anderson.jpg';
 import thomasAndersonMobile from 'assets/image/thomas_anderson_mobile.jpg';
 import justQuiz from 'assets/image/just_quiz.jpg';
@@ -51,8 +53,19 @@ const headingData = {
 };
 
 const websiteProjects = [
+    {
+        id: 1,
+        title: 'Music Festival',
+        content:
+          'Jest to jeden z moich najnowszych projektów, stworzyłem go aby odświeżyć portfolio. Layout strony stworzyłem w programie Figma, a następnie zakodowałem stronę przy użyciu techonologii wymienionych poniżej. Artyści są wczytywani z pliku JSON metodą FETCH API, a następnie za pomocą JavaScript dodani do strony. Strona posiada RWD. Arkusz stylów jest napisany przy użyciu preprocesora SASS. Zastosowałem tutaj metodę Mobile First',
+        technologiesContent:
+          'Użyte technologie: HTML, CSS(SASS & BEM), JavaScript, SplideJs, Webpack, Figma, Photoshop',
+        siteUrl: 'https://music-festival-cracow.netlify.app/',
+        image: musicFestival,
+        imageMobile: musicFestivalMobile,
+      },
   {
-    id: 1,
+    id: 2,
     title: 'Thomas Anderson',
     content:
       'Wstępny layout strony stworzyłem w programie Figma oraz grafiki w programie Photoshop. Postanowiłem użyc przy tym projekcie uprzednio skonfigurowanego Webpacka ponieważ chciałem przetestować go w praktyce. W projekcie zdecydowałem się użyc preprocesora SASS w połączeniu z metodologią BEM. Strona posiada wersję mobilną i jest w pełni responsywna (Desktop First).',
@@ -63,7 +76,7 @@ const websiteProjects = [
     imageMobile: thomasAndersonMobile,
   },
   {
-    id: 2,
+    id: 3,
     title: 'Portfolio',
     content:
       'Portfolio, które właśnie oglądasz jest najbardziej rozbudowanym projektem, który stworzyłem w ReactJS. Postanowiłem użyc Reacta ze względu na routing. Za pomocą CSSTransition udało mi się stworzyć animowane przejścia pomiędzy podstronami. Od samego początku tworzenia aplikacji używałem narzędzi takich jak Eslint oraz Prettier, które zkonfigurowałem w plikach .eslintrc oraz .prettierrc. Struktura plików jest zbudowana zgodnie z zasadą Atomic Design Pattern. Strone stylowałem za pomocą bilblioteki styled-components. Portfolio jest w pełni responsywne. Layout stworzony w programie Figma. ',
